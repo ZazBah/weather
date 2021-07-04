ff9c4f83a608ec73ead3de9c6541a57802124556
ZazBah <zazbah88@gmail.com>
2021-07-04 10:43:27 +0600

    temp converter added

--- asset/js/javascript.js
+++ asset/js/javascript.js
@@ -1,54 +1,66 @@
 window.onload = function (){
-    let City = 'Dhaka'
-    dataEntry(City)
+    let countOn = "'F";
+    let Scale = 'imperial';
+    let City = 'Dhaka';
+
+    dataEntry(City,Scale, countOn)
 }
 
 function handleSearch(){
-    let City = $('#inputCity').val()
-    dataEntry(City)
+    let countOn = "'F";
+    let Scale = 'imperial';
+    let inputCity = $('#inputCity').val()
+    let City = inputCity;
+
+    dataEntry(City,Scale,countOn)
 }
 
+function handleCelTmep(){
+    let countOn = "'C";
+    let Scale = 'metric';
+    let City = $('#city').text()
 
+    dataEntry(City,Scale,countOn)
+}
 
-function dataEntry(City){
-    $.get("https://api.openweathermap.org/data/2.5/weather?q="+ City +"&units=imperial&appid=9334788e9645c1afc1ad3644bb22e522", function(data){
-        var weatherDataF = {
-            "location" : data.name + "," + data.sys.country,
-            "temperature" : Math.ceil(data.main.temp) + "'F",
-            "icon" : "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png",
-            "weatherType" : data.weather[0].main,
-            "feelsLike" : "Feels Like " + Math.ceil(data.main.feels_like) + "'F",
-            "minTemp" : "Minimum Temperature " + Math.ceil(data.main.temp_min) + "'F",
-            "maxTemp" : "Maximum Temperature " + Math.ceil(data.main.temp_max) + "'F",
-            "humidity" : "Humidity " + data.main.humidity,
-            "clouds" : data.clouds.all,
-            "windSpeed" : "Speed " + data.wind.speed + "ms",
-            "windDirection" : "Direction " + data.wind.deg + "deg"
-        }
+function handleFerTmep(){
+    let countOn = "'F";
+    let Scale = 'imperial';
+    let City = $('#city').text()
 
-        var weatherDataC = {
-            "location" : data.name + "," + data.sys.country,
-            "temperature" : Math.ceil(( data.main.temp - 32 ) * 0.56 ) + "'C",
-            "icon" : "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png",
-            "weatherType" : data.weather[0].main,
-            "feelsLike" : "Feels Like " + Math.ceil(( data.main.feels_like - 32 ) * 0.56 ) + "'C",
-            "minTemp" : "Minimum Temperature " + Math.ceil(( data.main.temp_min - 32 ) * 0.56 ) + "'C" ,
-            "maxTemp" : "Maximum Temperature " + Math.ceil(( data.main.temp_max - 32 ) * 0.56 ) + "'C" ,
-            "humidity" : "Humidity " + data.main.humidity,
-            "clouds" : data.clouds.all,
-            "windSpeed" : "Speed " + data.wind.speed + "ms",
-            "windDirection" : "Direction " + data.wind.deg + "deg"
-        }
+    dataEntry(City,Scale,countOn)
+}
 
-        dataInput(weatherDataC)
-
+function dataEntry(City,Scale,countOn){
+    let count = countOn;
     
-});
+    $.get("https://api.openweathermap.org/data/2.5/weather?q="+ City +"&units="+ Scale +"&appid=9334788e9645c1afc1ad3644bb22e522", function dataSubmit(data,countOn){
+        var weatherData = {
+            "location" : data.name + "," + data.sys.country,
+            "city" : data.name,
+            "country" : data.sys.country,
+            "temperature" : Math.ceil(data.main.temp) + count,
+            "icon" : "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png",
+            "weatherType" : data.weather[0].main,
+            "feelsLike" : "Feels Like " + Math.ceil(data.main.feels_like) + count,
+            "minTemp" : "Minimum Temperature " + Math.ceil(data.main.temp_min) + count,
+            "maxTemp" : "Maximum Temperature " + Math.ceil(data.main.temp_max) + count,
+            "humidity" : "Humidity " + data.main.humidity,
+            "clouds" : data.clouds.all,
+            "windSpeed" : "Speed " + data.wind.speed + "ms",
+            "windDirection" : "Direction " + data.wind.deg + "deg"
+        }
+
+        dataInput(weatherData)
+    });
 }
 
 function dataInput(weatherData){
-    let location = weatherData.location ;
-    $('#location').text(location)
+    let city = weatherData.city ;
+    $('#city').text(city)
+
+    let country = weatherData.country ;
+    $('#country').text(country)
 
     let temperature =  weatherData.temperature;
     $('#temperature').text(temperature)
